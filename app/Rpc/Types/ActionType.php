395cb075504d672a61fb5b34dadeefc39776db9a
zouguofeng <243944672@qq.com>
2020-08-03 09:12:20 +0800

    更新

--- app/Rpc/Types/ActionType.php
+++ app/Rpc/Types/ActionType.php
@@ -10,6 +10,7 @@ final class ActionType
     const FORGET_PAY_PASSWORD = 'public.forget_pay_password';
     const SET_PAY_PASSWORD = 'public.set_pay_password';
     const CHANGE_PHONE_NUMBER = 'public.change_phone_number';
+    const KJ_BUY = 'machine.buy';
     static public $__names = array(
         'public.register' => 'REGISTER',
         'public.login' => 'LOGIN',
@@ -17,5 +18,6 @@ final class ActionType
         'public.forget_pay_password' => 'FORGET_PAY_PASSWORD',
         'public.set_pay_password' => 'SET_PAY_PASSWORD',
         'public.change_phone_number' => 'CHANGE_PHONE_NUMBER',
+        'machine.buy' => 'KJ_BUY',
     );
 }
